b98b4606b35fe4759d820fbf3217ffa0e8419466
Yutao <yutaogui@gmail.com>
2020-12-05 18:52:38 +0100

    Modify day1 solution

--- day1/day1.py
+++ day1/day1.py
@@ -1,20 +1,30 @@
-with open('day1.in') as f:
-    nums = [int(num) for line in f for num in line.split()]
+TARGET = 2020
 
-N = len(nums)
-for i in range(N):
-    for j in range(i+1, N):
-        a = nums[i]
-        b = nums[j] 
-        if a + b == 2020:
-            print(a*b)
-
-
-for i in range(N):
-    for j in range(i+1, N):
-        for k in range(j+1, N):
+def sum2(nums, target):
+    # Solution to part 1
+    N = len(nums)
+    for i in range(N):
+        for j in range(i+1, N):
             a = nums[i]
             b = nums[j]
-            c = nums[k]
-            if a + b + c == 2020:
-                print(a*b*c)
+            if a + b == target:
+                print(a*b)
+
+def sum3(nums, target):
+    # Solution to aprt 2
+    N = len(nums)
+    for i in range(N):
+        for j in range(i+1, N):
+            for k in range(j+1, N):
+                a = nums[i]
+                b = nums[j]
+                c = nums[k]
+                if a + b + c == target:
+                    print(a*b*c)
+
+if __name__ == "__main__":
+    with open('day1.in') as f:
+        nums = [int(num) for line in f for num in line.split()]
+
+    sum2(nums, TARGET)
+    sum3(nums, TARGET)
