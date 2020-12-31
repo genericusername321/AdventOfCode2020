4a677366954215723b82101a5faae773f2f493d6
Yutao Gui <yutaogui@gmail.com>
2020-12-31 23:17:44 +0100

    Day 11 part 2 solution

--- day11/day11.py
+++ day11/day11.py
@@ -1,4 +1,5 @@
 def findAdjacent(layout, i, j):
+    # Find adjacent cells
     height = len(layout)
     width = len(layout[0])
 
@@ -7,101 +8,98 @@ def findAdjacent(layout, i, j):
     
     neighbours = [line[j-1:j+2] for line in layout[i-1:i+2]]
     neighbours = [el for line in neighbours for el in line]
+
+    # Remove middle cell itself
     neighbours = neighbours[:4] + neighbours[5:]
 
     return neighbours
 
-def findInview(layout, i, j, height, width, cell):
-    # Find all cells that are visible from i,j, that is, all cells (x,y)
-    # such that x == i or y ==j, or x-i == y-j
+def findView(layout, i, j, height, width):
+    # Find the seats that are visible from location [i][j]
 
-    nOccupied = 0
-    # East
-    for k in range(i+1, width):
-        cell = layout[i][k]
-        if cell == 'L':
-            break
-        elif cell == '#':
-            nOccupied += 1
-            break
-        else:
-            layout[i][k] == 'X'
+    neighbours = []
+    locations = []
+    seats = {'L', '#'}
 
-    # South-East
-    d = min(height-i, width-j)
-    for k in range(1, d):
-        cell = layout[i+k][j+k]
-        if cell == 'L':
-            break
-        elif cell == '#':
-            nOccupied += 1
-            break
 
-    # South
-    for k in range(j+1, height):
+    # Look north
+    for k in range(i-1, 0, -1):
         cell = layout[k][j]
-        if cell == 'L':
-            break
-        elif cell == '#':
-            nOccupied += 1
-            break
-
-    # South-West
-    d = min(height-i, j+1)
-    for k in range(1,d):
-        cell = layout[i+k][j-k]
-        if layout[i+k][j-k] == 'L':
-            break
-        elif layout[i+k][j-k] == '#':
-            nOccupied += 1
-            break
-
-    # West
-    for k in range(j-1,-1):
-        cell = layout[i][k]
-        if cell == 'L':
-            break
-        elif cell == '#':
-            nOccupied += 1
-            break
-
-    # North-West
-    d = min(i+1, j+1)
-    for k in range(1, d):
-        cell = layout[i-k][j-k]
-        if cell == 'L':
-            break
-        elif cell == '#':
-            nOccupied += 1
-            break
-
-    # North
-    for k in range(i-1, -1):
-        cell = layout[k][j]
-        if cell == 'L':
-            break
-        elif cell == '#':
-            nOccupied += 1
-            break
-
-    # North-East
-    d = min(i+1, width-j)
-    for k in range(1, d):
-        cell = layout[i-k][j+k]
-        if cell == 'L':
-            break
-        elif cell == '#':
-            nOccupied += 1
+        if cell in seats:
+            neighbours.append(cell)
+            locations.append((k,j))
             break
     
-    return nOccupied
+    # Look east
+    for k in range(j+1, width):
+        cell = layout[i][k]
+        if cell in seats:
+            neighbours.append(cell)
+            locations.append((i,k))
+            break
+    
+    # Look south
+    for k in range(i+1, height):
+        cell = layout[k][j]
+        if cell in seats:
+            neighbours.append(cell)
+            locations.append((k,j))
+            break
 
-def nextRound(layout):
+    # Look west
+    for k in range(j-1, 0, -1):
+        cell = layout[i][k]
+        if cell in seats:
+            neighbours.append(cell)
+            locations.append((i,k))
+            break
+
+    # Look north-east
+    d = min(i, width - (j+1))
+    for k in range(1,d):
+        cell = layout[i-k][j+k]
+        if cell in seats:
+            neighbours.append(cell)
+            locations.append((i-k, j+k))
+            break
+
+    # Look south-east
+    d = min(height - (i+1), width - (j+1))
+    for k in range(1,d):
+        cell = layout[i+k][j+k]
+        if cell in seats:
+            neighbours.append(cell)
+            locations.append((i+k, j+k))
+            break
+
+    # Look south-west
+    d = min(height - (i+1), j)
+    for k in range(1,d):
+        cell = layout[i+k][j-k]
+        if cell in seats:
+            neighbours.append(cell)
+            locations.append((i+k, j-k))
+            break
+
+    # north-west
+    d = min(i, j)
+    for k in range(1,d):
+        cell = layout[i-k][j-k]
+        if cell in seats:
+            neighbours.append(cell)
+            locations.append((i-k, j-k))
+            break
+
+    return neighbours
+
+
+def iterate(layout):
 
     height = len(layout)
     width = len(layout[0]) 
     change = False
 
+    # Create next iteration
     nextLayout = [['.' for _ in range(width)] for _ in range(height)]
     for i in range(1, height-1):
         for j in range(1, width - 1):
@@ -110,8 +108,7 @@ def nextRound(layout):
 
             # Find neighbours for part 2
             cell = layout[i][j]
-            numOccupied = findInview(layout, i, j, height, width, cell)
-            neighbours = ['#' for _ in range(numOccupied)]
+            neighbours = findView(layout, i, j, height, width)
 
             if cell == '.':
                 nextLayout[i][j] = '.'
@@ -124,6 +121,7 @@ def nextRound(layout):
                     change = True
                 else:
                     nextLayout[i][j] = 'L'
+
             elif cell == '#':
                 # If there are 4 (5 for part 2) or more adjacent occupied seats, 
                 # seat becomes empty
@@ -155,16 +153,18 @@ def parseInput(infile):
 
 if __name__ == "__main__":
 
-    infile = "day11.test"
+    infile = "day11.in"
     layout = parseInput(infile)
 
-#    change = True
-#    while change:
-#        layout, change = nextRound(layout)
-#        print('----- next -----')
-#        for line in layout:
-#            print(''.join(line))
-#
-#
-#    counter = [1 for line in layout for el in line if el == '#']
-#    print(sum(counter))
+    print('----- original -----')
+    for line in layout:
+        print(''.join(line))
+
+
+    change = True
+    while change:
+        layout, change = iterate(layout)
+
+
+    counter = [1 for line in layout for el in line if el == '#']
+    print(sum(counter))
